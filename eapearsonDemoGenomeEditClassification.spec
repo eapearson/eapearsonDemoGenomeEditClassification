c925cf0f79838b6ef6b95dbac034dc1a746265e2
Erik Pearson <eapearson@lbl.gov>
2023-12-16 07:10:22 -0800

    My first commit

---
+++ eapearsonDemoGenomeEditClassification.spec
@@ -0,0 +1,31 @@
+/*
+A KBase module: eapearsonDemoGenomeEditClassification
+*/
+
+module eapearsonDemoGenomeEditClassification {
+    typedef structure {
+        string description;
+    } ChangeLogEntry;
+
+    typedef structure {
+        /* Will be passed to viewer */
+        string output_genome_ref;
+        list<ChangeLogEntry> change_log;
+
+        /* To identify the viewer */
+        string service_module_name;
+        string widget_name;
+    } UpdateGenomeClassificationMetadataResult;
+
+    typedef structure {
+        int workspace_id;
+        string genome_ref;
+        string output_genome_name; 
+        string scientific_name;
+    } UpdateGenomeClassificationMetadataParams;
+
+    /*
+        This example function accepts any number of parameters and returns results in a KBaseReport
+    */
+    funcdef update_genome_classification_metadata(UpdateGenomeClassificationMetadataParams params) returns (UpdateGenomeClassificationMetadataResult output) authentication required;
+};
---
+++ lib/eapearsonDemoGenomeEditClassification/eapearsonDemoGenomeEditClassificationImpl.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+#BEGIN_HEADER
+import logging
+import os
+
+from installed_clients.KBaseReportClient import KBaseReport
+
+#END_HEADER
+
+
+class eapearsonDemoGenomeEditClassification:
+    '''
+    Module Name:
+    eapearsonDemoGenomeEditClassification
+
+    Module Description:
+    A KBase module: eapearsonDemoGenomeEditClassification
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = ""
+    GIT_COMMIT_HASH = ""
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.shared_folder = config['scratch']
+        logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
+                            level=logging.INFO)
+        #END_CONSTRUCTOR
+        pass
+
+
+    def update_genome_classification_metadata(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of type
+           "UpdateGenomeClassificationMetadataParams" -> structure: parameter
+           "workspace_id" of Long, parameter "genome_ref" of String,
+           parameter "output_genome_name" of String, parameter
+           "scientific_name" of String
+        :returns: instance of type "UpdateGenomeClassificationMetadataResult"
+           -> structure: parameter "output_genome_ref" of String, parameter
+           "change_log" of list of type "ChangeLogEntry" -> structure:
+           parameter "description" of String, parameter "service_module_name"
+           of String, parameter "widget_name" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN update_genome_classification_metadata
+        # Fake it for now...
+
+        output = {
+            "output_genome_ref": params["genome_ref"],
+            "change_log": ["foo", "bar", "baz"]
+        }
+        #END update_genome_classification_metadata
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method update_genome_classification_metadata return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK",
+                     'message': "",
+                     'version': self.VERSION,
+                     'git_url': self.GIT_URL,
+                     'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
+        return [returnVal]
